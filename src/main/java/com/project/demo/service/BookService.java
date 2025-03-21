43012c6e91992c73d366b599fd65f2575dcc804d
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2025-03-21 04:37:40 +0300

    Transactional && Cache

--- src/main/java/com/project/demo/service/BookService.java
+++ src/main/java/com/project/demo/service/BookService.java
@@ -1,9 +1,12 @@
 package com.project.demo.service;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 import com.project.demo.model.Book;
 import com.project.demo.repository.BookRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
@@ -13,18 +16,23 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    @Cacheable("books")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
+    @Cacheable(value = "book", key = "#id")
     public Book getBookById(Long id) {
         return bookRepository.findById(id).orElse(null);
     }
 
+    @CacheEvict(value = "books", allEntries = true)
     public Book addBook(Book book) {
         return bookRepository.save(book);
     }
 
+    @CacheEvict(value = "book", key = "#id") // حذف التخزين المؤقت عند تحديث كتاب معين
+    @Transactional
     public Book updateBook(Long id, Book updatedBook) {
         return bookRepository.findById(id).map(book -> {
             book.setTitle(updatedBook.getTitle());
@@ -35,6 +43,8 @@ public class BookService {
         }).orElse(null);
     }
 
+    @CacheEvict(value = {"book", "books"}, key = "#id") // حذف البيانات المؤقتة عند الحذف
+    @Transactional
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
